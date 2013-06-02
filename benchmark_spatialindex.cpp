2014d876a0a850f0963acf17f0a7cdcf7c88ab00
Mateusz Loskot <mateusz@loskot.net>
2013-06-03 00:54:04 +0100

    Basic SpatialIndex sample for RTree

--- benchmark_spatialindex.cpp
+++ benchmark_spatialindex.cpp
@@ -1,5 +1,27 @@
+#include <cstdint>
+#include <cstdlib>
 #include <spatialindex/SpatialIndex.h>
+using namespace std;
+using namespace SpatialIndex;
 
 int main()
 {
+    try
+    {
+        id_type identifier;
+        uint32_t index_capacity = 20;
+        uint32_t leaf_capacity = index_capacity;
+        uint32_t dimension = 2;
+        IStorageManager* sm = StorageManager::createNewMemoryStorageManager();
+        ISpatialIndex* si = RTree::createNewRTree(*sm, 0.7, index_capacity, leaf_capacity, dimension, SpatialIndex::RTree::RV_RSTAR, identifier);
+    
+        delete si;
+        delete sm;
+        
+        return EXIT_SUCCESS;
+    }
+    catch ( ... )
+    {
+    }
+    return EXIT_FAILURE;
 }
