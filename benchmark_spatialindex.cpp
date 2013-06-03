8fe2aaf9c41e46b1820200d33549588315715315
Mateusz Loskot <mateusz@loskot.net>
2013-06-04 00:47:17 +0100

    Avoid manual memory management. Rename point_t to coord_array_t.

--- benchmark_spatialindex.cpp
+++ benchmark_spatialindex.cpp
@@ -1,7 +1,5 @@
-//#include <cstdint>
-#include <cstdlib>
-#include <spatialindex/SpatialIndex.h>
 #include "spatial_index_benchmark.hpp"
+#include <spatialindex/SpatialIndex.h>
 using namespace std;
 namespace si = SpatialIndex;
 
@@ -9,32 +7,30 @@ int main()
 {
     try
     {
-        auto n = 10U;
-        //auto const coords = sibench::generate_coordinates(n);
+        auto n = 1000U;
         auto const boxes = sibench::generate_boxes(n);
 
         uint32_t index_capacity = 20;
         uint32_t leaf_capacity = index_capacity;
         uint32_t dimension = 2;
         si::id_type index_id;
-        si::IStorageManager* sm = si::StorageManager::createNewMemoryStorageManager();
-        si::ISpatialIndex* si = si::RTree::createNewRTree(*sm, 0.7, index_capacity,
-            leaf_capacity, dimension, si::RTree::RV_RSTAR, index_id);
+        std::unique_ptr<si::IStorageManager> sm(si::StorageManager::createNewMemoryStorageManager());
+        std::unique_ptr<si::ISpatialIndex> si(si::RTree::createNewRTree(*sm, 0.7, index_capacity,
+            leaf_capacity, dimension, si::RTree::RV_RSTAR, index_id));
 
+        typedef std::array<double, 2> coord_array_t;
+        
         auto const s = boxes.size();
         for (std::size_t i = 0; i < s; ++i)
         {
-            auto const& box = boxes[0];
-            typedef std::array<double, 2> point_t;
-            point_t p1 = { std::get<0>(box), std::get<1>(box) };
-            point_t p2 = { std::get<2>(box), std::get<3>(box) };
+            auto const& box = boxes[i];
+            coord_array_t p1 = { std::get<0>(box), std::get<1>(box) };
+            coord_array_t p2 = { std::get<2>(box), std::get<3>(box) };
 
             si::id_type item_id(i);
             si::Region region(si::Point(p1.data(), p1.size()), si::Point(p2.data(), p2.size()));
             si->insertData(0, nullptr, region, item_id);
         }
-        delete si;
-        delete sm;
         
         return EXIT_SUCCESS;
     }
