2494f4a4ca3f34a985d37e80820fada5aa2f72b5
Mateusz Loskot <mateusz@loskot.net>
2013-06-03 17:20:38 +0100

    Add random boxes generator and container typedefs.

--- spatial_index_benchmark.hpp
+++ spatial_index_benchmark.hpp
@@ -9,6 +9,7 @@
 #ifndef MLOSKOT_SPATIAL_INDEX_BENCHMARK_HPP_INCLUDED
 #define MLOSKOT_SPATIAL_INDEX_BENCHMARK_HPP_INCLUDED
 #include <algorithm>
+#include <array>
 #include <cassert>
 #include <fstream>
 #include <iostream>
@@ -33,9 +34,10 @@ std::size_t max_iterations = 1000;
 //
 // Generators of random objects
 //
+typedef std::vector<std::tuple<float, float>> coords_t;
+typedef std::vector<std::tuple<float, float, float, float>> boxes_t;
 
-
-inline std::vector<std::pair<float, float>> generate_coordinates(std::size_t n)
+inline coords_t generate_coordinates(std::size_t n)
 {
     float const max_val = static_cast<float>(n / 2);
 
@@ -43,11 +45,34 @@ inline std::vector<std::pair<float, float>> generate_coordinates(std::size_t n)
     std::mt19937 rgen(rdev());
     std::uniform_real_distribution<float> rdist(-max_val, max_val);
 
-    std::vector<std::pair<float, float>> coords;
+    coords_t coords;
     coords.reserve(n);
-    coords.emplace_back(rdist(rgen), rdist(rgen));
+    for (decltype(n) i = 0; i < n; ++i)
+    {
+        coords.emplace_back(rdist(rgen), rdist(rgen));
+    }
     return std::move(coords);
 }
+
+inline boxes_t generate_boxes(std::size_t n)
+{
+    float const max_val = static_cast<float>(n / 2);
+
+    std::random_device rdev;
+    std::mt19937 rgen(rdev());
+    std::uniform_real_distribution<float> rdist(-max_val, max_val);
+
+    boxes_t boxes;
+    boxes.reserve(n);
+    for (decltype(n) i = 0; i < n; ++i)
+    {
+        auto const x = rdist(rgen);
+        auto const y = rdist(rgen);
+        boxes.emplace_back(x - 0.5f, y - 0.5f, x + 0.5f, y + 0.5f);
+    }
+    return std::move(boxes);   
+}
+
 
 //
 // Benchmark running routines
