b467c51bc1ff74625ef3aaf99494c1d8010f99ba
Mateusz Loskot <mateusz@loskot.net>
2013-06-04 17:02:31 +0100

    Prpare for measuring multi-stage process: insert, query, etc.

--- benchmark_spatialindex.cpp
+++ benchmark_spatialindex.cpp
@@ -7,9 +7,16 @@ int main()
 {
     try
     {
+        auto print_status = [](sibench::result_info const& r)
+        {
+            std::cout << "spatialindex: " << r;
+        };
+
+        // Generate random objects for indexing
         auto n = 100U;
         auto const boxes = sibench::generate_boxes(n);
 
+        // Set up index
         uint32_t index_capacity = 20;
         uint32_t leaf_capacity = index_capacity;
         uint32_t dimension = 2;
@@ -18,23 +25,39 @@ int main()
         std::unique_ptr<si::ISpatialIndex> rtree(si::RTree::createNewRTree(*sm, 0.7, index_capacity,
             leaf_capacity, dimension, si::RTree::RV_RSTAR, index_id));
 
-        auto const marks = sibench::benchmark("insertions", sibench::max_iterations, boxes,
-            [&rtree] (sibench::boxes2d_t const& boxes) {
+        // Benchmark: insert
+        {
+            auto const marks = sibench::benchmark("insert", sibench::max_iterations, boxes,
+                [&rtree] (sibench::boxes2d_t const& boxes) {
 
-            typedef std::array<double, 2> coord_array_t;
-            auto s = boxes.size();
-            for (decltype(s) i = 0; i < s; ++i) {
-                auto const& box = boxes[i];
-                coord_array_t p1 = { std::get<0>(box), std::get<1>(box) };
-                coord_array_t p2 = { std::get<2>(box), std::get<3>(box) };
+                    typedef std::array<double, 2> coord_array_t;
+                    auto s = boxes.size();
+                    for (decltype(s) i = 0; i < s; ++i) {
+                        auto const& box = boxes[i];
+                        coord_array_t p1 = { std::get<0>(box), std::get<1>(box) };
+                        coord_array_t p2 = { std::get<2>(box), std::get<3>(box) };
 
-                si::id_type item_id(i);
-                si::Region region(si::Point(p1.data(), p1.size()), si::Point(p2.data(), p2.size()));
-                rtree->insertData(0, nullptr, region, item_id);
-            }
-        });
+                        si::id_type item_id(i);
+                        si::Region region(
+                            si::Point(p1.data(), p1.size()),
+                            si::Point(p2.data(), p2.size()));
+                        rtree->insertData(0, nullptr, region, item_id);
+                    }
+            });
+            print_status(marks);
+        }
+
+        // Benchmark: query
+        {
+            // TODO: benchmark() should forward query parameter, not boxes
+            auto const marks = sibench::benchmark("insert", sibench::max_iterations, boxes,
+                [&rtree] (sibench::boxes2d_t const& boxes) {
+                    ; // TODO
+            });
+            print_status(marks);
+        }
+
         
-        std::cout << "spatialindex: " << marks;
         return EXIT_SUCCESS;
     }
     catch (std::exception const& e)
