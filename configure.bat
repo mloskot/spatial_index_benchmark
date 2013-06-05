7b60208923aa8f83f5e9e7dce760befc9965b646
Mateusz Loskot <mateusz@loskot.net>
2013-06-05 12:45:32 +0100

    Do not enter build directory

--- configure.bat
+++ configure.bat
@@ -2,11 +2,11 @@
 set BLD=_build
 IF NOT EXIST %~dp0\CMakeLists.txt GOTO :NOTSRC
 IF NOT EXIST %~dp0\%BLD% mkdir %BLD%
-cd _build
+pushd _build
 cmake -G "Visual Studio 11" ^
     -DBOOST_ROOT=D:\dev\boost\_svn\trunk ^
     ..
-
+popd
 GOTO :EOF
 
 :NOTSRC
