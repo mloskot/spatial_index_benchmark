c77b638548ae2c4aca6e5eff619ad16b7f9aeb46
Mateusz Loskot <mateusz@loskot.net>
2013-06-06 22:49:51 +0100

    Report GCC and clang versions

--- bin/ci/before_install.sh
+++ bin/ci/before_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Installs requirements for spatial_index_benchmark
 source ./bin/ci/common.sh
-pwd
 echo "$(tmstamp) *** before_install::apt-get starting $(date) ***"
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
 sudo apt-get update -qq
 sudo apt-get install -qq libspatialindex-dev subversion
 echo "$(tmstamp) *** before_install::apt-get finished $(date) ***"
-
+gcc --version
+clang --version
 echo "$(tmstamp) *** before_install::svn co boost starting $(date) ***"
 # Boost 1.55+ or trunk is required
 mkdir -p ${BOOST_PREFIX}
