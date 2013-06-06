03e2632557c9f8c87d57e28eab166995688c6242
Mateusz Loskot <mateusz@loskot.net>
2013-06-06 01:14:46 +0100

    Fix working dir for cmake execution

--- configure.sh
+++ configure.sh
@@ -119,7 +119,5 @@ msg "Running CMake in build directoru '$BUILD_DIR'"
 msg "CMake options:"
 msg " ${CMAKE_OPTIONS}"
 
-cd $BUILD_DIR
-echo $PWD
-cmake $CMAKE_OPTIONS "$SRC_DIR"
+(cd $BUILD_DIR; cmake $CMAKE_OPTIONS "$SRC_DIR")
 
