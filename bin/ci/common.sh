6a088d85af775c4402f6b9121add37cbd4dea645
Mateusz Loskot <mateusz@loskot.net>
2013-06-16 02:31:26 +0100

    Disable multi-processing for make

--- bin/ci/common.sh
+++ bin/ci/common.sh
@@ -11,6 +11,7 @@ if [[ -f /sys/devices/system/cpu/online ]]; then
 	# Calculates 1.5 times physical threads
 	NUMTHREADS=$(( ( $(cut -f 2 -d '-' /sys/devices/system/cpu/online) + 1 ) * 15 / 10  ))
 fi
+NUMTHREADS=1 # disable MP
 export NUMTHREADS
 export BOOST_SVN="http://svn.boost.org/svn/boost/trunk/boost"
 export BOOST_PREFIX="${TRAVIS_BUILD_DIR}/trunk"
