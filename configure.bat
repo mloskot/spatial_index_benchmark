4231b6a6bfffdfecc128471f3c4b42208d6b1870
Mateusz Loskot <mateusz@loskot.net>
2013-06-20 13:25:11 +0100

    Set BGI_ENABLE_CT by default

--- configure.bat
+++ configure.bat
@@ -4,6 +4,7 @@ IF NOT EXIST %~dp0\CMakeLists.txt GOTO :NOTSRC
 IF NOT EXIST %~dp0\%BLD% mkdir %BLD%
 pushd _build
 cmake -G "Visual Studio 11" ^
+    -DBGI_ENABLE_CT=YES ^
     -DBOOST_ROOT=D:\dev\boost\_svn\trunk ^
     ..
 popd
